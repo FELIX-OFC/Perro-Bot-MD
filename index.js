148250d76cea7e74368df972f072b75f62a951d0
meado-learner <meadonix@gmail.com>
2025-10-03 15:55:33 -0600

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ import fs, { readdirSync, statSync, unlinkSync, existsSync, mkdirSync, readFileS
 import yargs from 'yargs'
 import { spawn } from 'child_process'
 import lodash from 'lodash'
-import { yukiJadiBot } from './plugins/sockets-serbot.js'
+import { perroJadiBot } from './plugins/sockets-serbot.js'
 import chalk from 'chalk'
 import syntaxerror from 'syntax-error'
 import pino from 'pino'
